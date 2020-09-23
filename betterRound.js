31421e3bf85f2995c7a2ef466f4ae909b15a4080
Paul <68358717+adkissonpaul@users.noreply.github.com>
2020-09-22 20:43:41 -0400

    Update betterRound.js

--- betterRound.js
+++ betterRound.js
@@ -1,47 +1,32 @@
-function moveDecimal (number, precision) {
-  var numberString = number.toString(); 
-  var decimalIndex = numberString.indexOf(".");
-
-  var noDecimal = numberString.slice(0, decimalIndex) + numberString.slice(decimalIndex + 1, numberString.length);
-  var newDecimal = noDecimal.slice(0, decimalIndex + precision) + '.' + noDecimal.slice(decimalIndex + precision, noDecimal.length);
-  return newDecimal;
-};
-
-function tryAgain(number, precision) {
-  precision = Math.pow(10, precision);
-  return (Math.round(number * precision))/precision;  
-}
-
-
 function betterRound(number, precision) {
   var numberString = number.toString(); 
   var decimalIndex = numberString.indexOf(".");
   var decimalLength = numberString.length - decimalIndex - 1;
   var decimalDifference = precision - decimalLength;
   
-  if(!numberString.indexOf('.')) {
-  	newDecimal = number * precision;
+  if(precision === undefined) {
+    var precision = 0;
   }
 
   if(decimalIndex < 0){
-  	decimalLength = 0
-  	decimalIndex = numberString.length;
-  	decimalDifference = precision - 0;
+    decimalLength = 0
+    decimalIndex = numberString.length;
+    decimalDifference = precision - 0;
   }
 
   var noDecimal = numberString.slice(0, decimalIndex) + numberString.slice(decimalIndex + 1, numberString.length);
 
   if(decimalLength < precision) {
-  	noDecimal = noDecimal.concat('0'.repeat(decimalDifference))
-  	return noDecimal.slice(0, decimalIndex) + '.' + noDecimal.slice(decimalIndex, noDecimal.length)
+    noDecimal = noDecimal.concat('0'.repeat(decimalDifference))
+    return noDecimal.slice(0, decimalIndex) + '.' + noDecimal.slice(decimalIndex, noDecimal.length)
   };
 
   var newDecimal = noDecimal.slice(0, decimalIndex + precision) + '.' + noDecimal.slice(decimalIndex + precision, noDecimal.length);
   
-  precision = Math.pow(10, precision);
+  precision = "1".concat('0'.repeat(precision));
   
   if(Math.floor(number) === number) {
-	newDecimal = number * precision;
+  newDecimal = number * precision;
   }
    
   return (Math.round(newDecimal))/precision;  
